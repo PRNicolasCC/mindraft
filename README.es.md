8bd0aed0f1d161113148d55263a51df6a6e95d0b
Nicolas Cueca <n800912@gmail.com>
2025-11-21 14:47:38 -0500

    Adición de archivo SQL con estructura de la base de datos utilizada en el proyecto y modificación de archivos README

--- README.es.md
+++ README.es.md
@@ -2,22 +2,22 @@
 
 Mindraft es una aplicación moderna para tomar notas que permite a los usuarios crear, editar y organizar apuntes dentro de diferentes cuadernos.  
 El objetivo del proyecto es ofrecer una interfaz sencilla e intuitiva para gestionar información personal, ideas, notas de estudio y más.
+En la raíz del proyecto se encuentra el archivo 'notas_db.sql' el cual contiene la estructura de la base de datos utilizada en el proyecto.
 
 ## 🚀 Funcionalidades actuales
 - Registro de usuarios
 - Inicio de sesión y manejo seguro de sesiones
 - Recuperación de contraseña por correo
+- Crear y administrar cuaderno
+- Crear, editar y eliminar notas
 
 ## 🛠️ Funcionalidades próximas
-- Crear, editar y eliminar notas
-- Crear y administrar cuadernos
-- Etiquetado y categorización de notas
-- Sistema de búsqueda
-- Modo claro/oscuro
-- Sincronización en la nube
+- Exportar notas a PDF y Word
+- Búsqueda de cuadernos específicos
+- Integración ed IA para resumen y análisis de notas
 
 ## 📌 Estado del proyecto
-🧱 El proyecto se encuentra en fase inicial. El módulo de autenticación está completo, y el siguiente paso es implementar la gestión de notas y cuadernos.
+🧱 El proyecto se encuentra en producción para su primera versión. Las funciones principales de autenticación y la gestión de notas están completadas, y los siguientes pasos incluyen implementar la exportación a PDF y Word, así como la integración de IA para resúmenes y análisis de las notas.
 
 ## 🌐 Otros lenguages
 - 🇬🇧 English: README.md 
---
+++ notas_db.sql
@@ -0,0 +1,259 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 21-11-2025 a las 20:25:42
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `notas_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cuadernos`
+--
+
+CREATE TABLE `cuadernos` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(75) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `estado_id` char(1) DEFAULT 'A',
+  `descripcion` varchar(100) DEFAULT NULL,
+  `color` char(7) DEFAULT NULL,
+  `ultima_modificacion` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalles`
+--
+
+CREATE TABLE `detalles` (
+  `id` int(11) NOT NULL,
+  `descripcion` longtext DEFAULT NULL,
+  `nota_id` int(11) NOT NULL COMMENT 'esta columna es una llave foránea, sin embargo, como también es una columna con valores únicos, en el listado de índices únicamente se muestra el índice UNIQUE.'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estados`
+--
+
+CREATE TABLE `estados` (
+  `id` char(1) NOT NULL,
+  `nombre` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `notas`
+--
+
+CREATE TABLE `notas` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(75) NOT NULL,
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp(),
+  `cuaderno_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tokens`
+--
+
+CREATE TABLE `tokens` (
+  `id` int(11) NOT NULL,
+  `token` char(64) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `expires_at` timestamp NULL DEFAULT NULL,
+  `used` tinyint(1) DEFAULT 0,
+  `used_at` timestamp NULL DEFAULT NULL,
+  `ip_address` varchar(45) DEFAULT NULL,
+  `tipo` char(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `token_tipos`
+--
+
+CREATE TABLE `token_tipos` (
+  `id` char(1) NOT NULL,
+  `nombre` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `email` varchar(75) NOT NULL,
+  `contraseña` varchar(255) NOT NULL,
+  `is_active` tinyint(1) NOT NULL DEFAULT 0,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `last_login` timestamp NULL DEFAULT NULL,
+  `password_changed_at` timestamp NULL DEFAULT NULL,
+  `activated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `cuadernos`
+--
+ALTER TABLE `cuadernos`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `unique_nombre` (`nombre`),
+  ADD KEY `fk_usuario` (`usuario_id`),
+  ADD KEY `fk_estado` (`estado_id`);
+
+--
+-- Indices de la tabla `detalles`
+--
+ALTER TABLE `detalles`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `unique_nota` (`nota_id`);
+
+--
+-- Indices de la tabla `estados`
+--
+ALTER TABLE `estados`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `unique_nombre` (`nombre`);
+
+--
+-- Indices de la tabla `notas`
+--
+ALTER TABLE `notas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_cuaderno` (`cuaderno_id`);
+
+--
+-- Indices de la tabla `tokens`
+--
+ALTER TABLE `tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idx_token` (`token`) USING BTREE,
+  ADD KEY `fk_usuario_id` (`usuario_id`),
+  ADD KEY `fk_tipo` (`tipo`);
+
+--
+-- Indices de la tabla `token_tipos`
+--
+ALTER TABLE `token_tipos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `unique_email` (`email`),
+  ADD KEY `idx_active` (`is_active`),
+  ADD KEY `idx_nombre` (`nombre`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `cuadernos`
+--
+ALTER TABLE `cuadernos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `detalles`
+--
+ALTER TABLE `detalles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `notas`
+--
+ALTER TABLE `notas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tokens`
+--
+ALTER TABLE `tokens`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `cuadernos`
+--
+ALTER TABLE `cuadernos`
+  ADD CONSTRAINT `fk_estado` FOREIGN KEY (`estado_id`) REFERENCES `estados` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_usuario` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `detalles`
+--
+ALTER TABLE `detalles`
+  ADD CONSTRAINT `fk_nota` FOREIGN KEY (`nota_id`) REFERENCES `notas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `notas`
+--
+ALTER TABLE `notas`
+  ADD CONSTRAINT `fk_cuaderno` FOREIGN KEY (`cuaderno_id`) REFERENCES `cuadernos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `tokens`
+--
+ALTER TABLE `tokens`
+  ADD CONSTRAINT `fk_tipo` FOREIGN KEY (`tipo`) REFERENCES `token_tipos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_usuario_id` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
+
+INSERT INTO `estados` (`id`, `nombre`) VALUES
+('A', 'Activo'),
+('I', 'Inactivo');
+
+INSERT INTO `token_tipos` (`id`, `nombre`) VALUES
+('P', 'password_reset'),
+('A', 'account_activation');
+
